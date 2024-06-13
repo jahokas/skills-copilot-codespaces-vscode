f0c163e9a0442746d811d99792f262ad4cc27d21
jahokas <toteemitutkimus@gmail.com>
2024-06-13 19:16:38 +0300

    Copilot second commit

---
+++ .devcontainer/member.js
@@ -0,0 +1,20 @@
+function skillsMember() {
+  return [
+    {
+      name: 'Vim',
+      level: '80%'
+    },
+    {
+      name: 'Docker',
+      level: '60%'
+    },
+    {
+      name: 'Kubernetes',
+      level: '50%'
+    },
+    {
+      name: 'Jenkins',
+      level: '40%'
+    }
+  ]
+}
